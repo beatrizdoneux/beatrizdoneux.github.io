2a19ff36f5e51349dbb52f68b1ae7d6c7990b001
Maria Beatriz Camargo <mbeatrizcamargo@hotmail.com>
2024-08-28 16:19:59 -0700

    feat: adds footer

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App container-fluid p-0">
       <Navbar />
       <Hero />
-      
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <div className="container=fluid text-center fixed-bottom border-top py-2">
+            <p className="m-0">&copy; Beatriz Doneux 2024</p>
+        </div>
+    )
+}
+
+export default Footer
