79069b5549ac3795ec4b830bbf0f27d635143dda
Maria Beatriz Camargo <mbeatrizcamargo@hotmail.com>
2025-07-28 13:17:50 -0700

    docs: corrects base in vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/beatrizdoneux.github.io/',
+  // base: '/beatrizdoneux.github.io/',
   build: {
     outDir: 'docs',  // important for GitHub Pages deployment from main/docs
   },
